313c3609cf28d857a8c42622e49aa4aa45f4254f
bartosz.janiak <bartosz.janiak@aug-e.io>
2024-07-01 17:24:54 +0200

    initial commit. add pretier,jest,eslint, tailwind basic config

--- src/app/shared/utilities/no-op-value-accessor.ts
+++
@@ -1,7 +0,0 @@
-import { ControlValueAccessor } from '@angular/forms';
-
-export const NOOP_VALUE_ACCESSOR: ControlValueAccessor = {
-  writeValue: () => {},
-  registerOnChange: () => {},
-  registerOnTouched: () => {}
-};
